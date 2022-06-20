563f6258cd3a95003132f5ebdc9e00524779220b
Arjun <arjun140702@gmail.com>
2022-06-21 00:17:16 +0530

    Add function to map routes to Express router

---
+++ lib/router/map.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import path from "path"
+
+const router = express.Router()
+
+export default (dir: string, paths: string[], routes: string[]) => {
+    routes.forEach((route, index) => {
+        const cwd = path.relative(__dirname, dir)
+        router.all(`/${route}`, require(path.join(cwd, paths[index])).default)
+    })
+}
