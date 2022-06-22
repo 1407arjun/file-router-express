5e1710d77353c28c6d1ea53e0f14e948eb3060cf
Arjun <arjun140702@gmail.com>
2022-06-22 20:11:05 +0530

    Handle exports for default set of HTTP requests

--- lib/router.ts
+++ lib/router.ts
@@ -13,7 +13,13 @@ const { paths, routes } = mapRoutes(files)
 routes.forEach((route, index) => {
     const cwd = path.relative(__dirname, dir)
     const handlers = require(path.join(cwd, paths[index]))
+
     handlers.default && router.all(`/${route}`, handlers.default)
+    handlers.get && router.get(`/${route}`, handlers.get)
+    handlers.post && router.post(`/${route}`, handlers.post)
+    handlers.put && router.put(`/${route}`, handlers.put)
+    handlers.patch && router.patch(`/${route}`, handlers.patch)
+    handlers.del && router.delete(`/${route}`, handlers.del)
 })
 
 export default router
